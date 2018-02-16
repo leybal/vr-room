7d538adbd1767136b716f429f3f52374b66209bc
leybal <kravchuk018@gmail.com>
2018-02-16 21:52:28 +0200

    back to first commit

--- server/app.js
+++ server/app.js
@@ -8,8 +8,6 @@ const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-console.log(__dirname);
-
 app.use(express.static(path.join(__dirname, '/../static_assets')));
 
 
